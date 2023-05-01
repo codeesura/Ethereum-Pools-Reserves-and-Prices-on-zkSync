4eca17e684d87e127b48147921d467659fec20ba
codeesura <120671243+codeesura@users.noreply.github.com>
2023-05-02 00:05:43 +0300

    Create deploy.ts

---
+++ deploy/deploy.ts
@@ -0,0 +1,18 @@
+import { Wallet, utils } from "zksync-web3";
+import * as ethers from "ethers";
+import { HardhatRuntimeEnvironment } from "hardhat/types";
+import { Deployer } from "@matterlabs/hardhat-zksync-deploy";
+
+// An example of a deploy script that will deploy and call a simple contract.
+export default async function (hre: HardhatRuntimeEnvironment) {
+  console.log(`Running deploy script for the Greeter contract`);
+
+  // Initialize the wallet.
+  const wallet = new Wallet("PRIVATE_KEY");
+
+  // Create deployer object and load the artifact of the contract you want to deploy.
+  const deployer = new Deployer(hre, wallet);
+  const artifact = await deployer.loadArtifact("MyContract");
+  const deploymentFee = await deployer.estimateDeployFee(artifact, []);
+  const parsedFee = ethers.utils.formatEther(deploymentFee.toString());
+  console.log(`The deployment is estimated to cost ${parsedFee} ETH`);
